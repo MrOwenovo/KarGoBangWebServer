b5618e5d0d5df00c921240d69b0a118c476c4fba
MrOwen <lmq122677@qq.com>
2023-12-13 21:24:16 +0800

    kts debug

--- .space.kts
+++ .space.kts
@@ -1,25 +1,21 @@
-
 job("Build and Deploy") {
     container(image = "docker") {
-        env["DOCKER_USERNAME"] = "{{ env.DOCKER_USERNAME }}"
-        env["DOCKER_PASSWORD"] = "{{ env.DOCKER_PASSWORD }}"
+        env["DOCKER_USERNAME"] = "{{ project:DOCKER_USERNAME }}"
+        env["DOCKER_PASSWORD"] = "{{ project:DOCKER_PASSWORD }}"
         shellScript {
             content = """
                 # 构建 Docker 镜像 
                 docker build -t kargobangapp:latest .
                 
-              
                 # 推送镜像到 Docker 仓库
-                # 假设您已在项目的环境变量中配置了 DOCKER_USERNAME  DOCKER_PASSWORD
                 echo "${'$'}DOCKER_PASSWORD" | docker login -u "${'$'}DOCKER_USERNAME" --password-stdin
                 docker tag kargobangapp:latest mrowenovo/kargobang:latest
                 docker push mrowenovo/kargobang:latest
 
                 # 使用 docker-compose 启动生产环境容器
-                # 假设您的 compose.prod.yaml 在项目根目录
                 docker-compose -f compose.prod.yaml up -d
             """
         }
     }
-    
+
 }
